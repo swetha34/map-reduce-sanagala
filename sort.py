3b737521e0c6ccb7eaab6d6ccf092011b3bc65d1
swetha34 <77765985+swetha34@users.noreply.github.com>
2022-01-28 19:21:42 -0600

    final output generated

---
+++ sort.py
@@ -0,0 +1,10 @@
+n = open("sanagala.txt","r")  # open file, read-only
+s = open("s.txt", "w") # open file, write
+lines = n.readlines()
+lines.sort()
+
+for line in lines:
+ s.write(line)
+
+n.close()
+s.close()
